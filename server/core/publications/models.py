6fd4c3e854aba64a4320835ef1ded9f1d16184b3
grizlle <61758804+grizlle@users.noreply.github.com>
2023-03-31 15:19:45 +0300

    Добавление полей

--- server/core/publications/models.py
+++ server/core/publications/models.py
@@ -23,7 +23,23 @@ class Publication(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     file_url = models.FileField(upload_to="publications/files/%Y/%m/%d", blank=True, null=True)
     authors = models.ManyToManyField(CustomUser, related_name="publication_list")
-
+    #Правки
+    WoS_CC =  models.BooleanField(default=False)
+    scopus = models.BooleanField(default=False)
+    RINC = models.BooleanField(default=False)
+    elib_ID = models.PositiveIntegerField()
+    if_index_export = models.TextField(blank=True)
+    date_publ = models.DateTimeField(auto_now=False)
+    DOI = models.TextField(blank=True)
+    ISSN = models.TextField(blank=True)
+    e-ISSN = models.TextField(blank=True)
+    ISBN = models.TextField(blank=True)
+    qwart_izd = models.DateField(blank=True, null=True)
+    affil = models.TextField(blank=True)
+    finans = models.TextField(blank=True)
+    publ_type = models.TextField(blank=True)
+    
+    
     def __str__(self):
         return self.title
 
