c4a3ea5d617464e8b24e4229a8056a617d470516
grizlle <61758804+grizlle@users.noreply.github.com>
2023-05-04 16:23:26 +0300

    Update models.py
    Добавил verbose_name

--- server/core/users/models.py
+++ server/core/users/models.py
@@ -8,21 +8,17 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    last_name = models.CharField("Фамилия", max_length=150, blank=False, null=False)
-    first_name = models.CharField("Имя", max_length=150, blank=False, null=False)
-    patronymic = models.CharField("Отчество", max_length=150, blank=False, null=False)
-    fio = models.CharField('ФИО', max_length=255, default='', blank=True)
-    avatar_url = models.ImageField(upload_to="users/files/%Y/%m/%d", blank=True, null=True)
+    last_name = models.CharField("Фамилия", max_length=150, blank=False, null=False, verbose_name = 'Фамилия')
+    first_name = models.CharField("Имя", max_length=150, blank=False, null=False, verbose_name = 'Имя')
+    patronymic = models.CharField("Отчество", max_length=150, blank=False, null=False, verbose_name = 'Отчество')
+    fio = models.CharField('ФИО', max_length=255, default='', blank=True, verbose_name = 'ФИО')
+    avatar_url = models.ImageField(upload_to="users/files/%Y/%m/%d", blank=True, null=True, verbose_name = 'URL аватарки')
     #Правки
-    wos_id = models.PositiveIntegerField(blank=True, default='1')
-    scorpus_auth_id = models.PositiveIntegerField(blank=True, default='1')
-    orcid = models.PositiveIntegerField(blank=True, default='1')
-    spin_kod = models.PositiveIntegerField(blank=True, default='1')
-    rinc_auth_id = models.PositiveIntegerField(blank=True, default='1')
-    
-    
-    
-    
+    wos_id = models.PositiveIntegerField(blank=True, default='1', verbose_name = 'WoS Researcher ID члена научного коллектива')
+    scorpus_auth_id = models.PositiveIntegerField(blank=True, default='1', verbose_name = 'Scopus AuthorID члена научного коллектива')
+    orcid = models.PositiveIntegerField(blank=True, default='1', verbose_name = 'ORCID члена научного коллектива')
+    spin_kod = models.PositiveIntegerField(blank=True, default='1', verbose_name = 'SPIN-код члена науного коллектива')
+    rinc_auth_id = models.PositiveIntegerField(blank=True, default='1', verbose_name = 'РИНЦ AuthorID члена научного коллектива')
     
     def __str__(self):
         return self.username
